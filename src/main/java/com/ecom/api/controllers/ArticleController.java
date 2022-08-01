d79a7b0dce469e7fb9576f025f7e304e823e5cc6
unknown <Oumardev>
2022-08-01 11:23:25 +0000

    feat exception

--- src/main/java/com/ecom/api/controllers/ArticleController.java
+++ src/main/java/com/ecom/api/controllers/ArticleController.java
@@ -1,6 +1,5 @@
 package com.ecom.api.controllers;
 
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
--- src/main/java/com/ecom/api/controllers/UserController.java
+++ src/main/java/com/ecom/api/controllers/UserController.java
@@ -2,7 +2,6 @@ package com.ecom.api.controllers;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ecom.api.exception.ApiBadRequestionExceptionHandler;
 import com.ecom.api.exception.ApiRequestException;
 import com.ecom.api.models.User;
 import com.ecom.api.services.UserService;
